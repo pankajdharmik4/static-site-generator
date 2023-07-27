0f2dcf43c2198d87e885f5dfb7e15af6332ccb21
Pankaj Dharmik <pankajdharmik1999@gmail.com>
2023-07-27 19:33:32 +0530

    Desktop Frontend Page Done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        lexendDeca: ['Lexend Deca']
+      },
+      colors:{
+        primary: '#2B2B35',
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
